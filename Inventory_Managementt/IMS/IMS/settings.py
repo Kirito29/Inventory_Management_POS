d462c8c060c33856b0cf08eee19aecf8a149f1fe
Kirito29 <yakimianandrew@outlook.com>
2021-07-24 13:10:05 -0400

    DB configuration

--- Inventory_Managementt/IMS/IMS/settings.py
+++ Inventory_Managementt/IMS/IMS/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'IMSapp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'IMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'IMSappDB',
+        'USER': 'postgres',
+        'PASSWORD': 'IMSappDBPW',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
